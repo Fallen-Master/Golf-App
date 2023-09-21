31bb4fe40bb442fdaf27627d77ef8265cb9d60b6
Rafael Gomez <rgomez2014@gmail.com>
2023-09-21 10:11:11 -0500

    added course name and score
    added defuat value for date as NOW

--- models/round.js
+++ models/round.js
@@ -11,11 +11,19 @@ Round.init(
             primaryKey:true,
             autoIncrement: true,
         },
+        courseName:{
+            type: DataType.STRING,
+            allowNull: false,
+        },
         date:{
             type: DataTypes.DATE,
-            type: DataTypes.NOW,
+            defaultValue: DataTypes.NOW,
             allowNull:false,
-
         },
+        score:{
+            type: DataTypes.INTEGER,
+            allowNull:false,
+        },
+        
     }
 )
