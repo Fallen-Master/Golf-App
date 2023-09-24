5be6f77f58066b995bfc84a235933ea076082e67
Rafael Gomez <rgomez2014@gmail.com>
2023-09-24 14:09:24 -0500

    history and log in added

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
   res.render('homepage');
 });
 
-router.get('/club', withAuth, async (req, res) => {
+router.get('/clubRanges', withAuth, async (req, res) => {
   try {
     const clubData = await User.findByPk(req.session.user_id, {
       include: [
@@ -16,13 +16,42 @@ router.get('/club', withAuth, async (req, res) => {
         }
       ]
     })
-    const club = clubData.Club.map((club) => this.post.get({ plain: true }));
+    const clubinfo = clubData.map((cl) => cl.get({ plain: true }));
     res.render('dashboard', {
-      club,
+      clubinfo,
       logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get('/history', withAuth, async (req, res) => {
+  try {
+    const historyData = await User.findByPk(req.session.user_id, {
+      include: [
+        {
+          model: Round,
+          attributes: ['courseName', 'date', 'score', 'par', 'comment']
+        }
+      ]
+    })
+    const history = historyData.map((his) => his.get({ plain: true }));
+    res.render('history', {
+      history,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.get('/login', (req, res) => { 
+  if (req.session.logged_in) {// if user is logged in it takes them to the thier profile
+    res.redirect('/history');
+    return;
+  }
+  res.render('login');
+});
+
 module.exports = router;
