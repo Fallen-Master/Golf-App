854aa02b4789e53cc49313c058b4a9ce748fa8a6
Rafael Gomez <rgomez2014@gmail.com>
2023-09-24 19:52:17 -0500

    added allownull false

--- models/club.js
+++ models/club.js
@@ -21,6 +21,7 @@ Club.init(
         },
         user_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             references: {
                 model: 'user',
                 key: 'id',
