99e044d985f8599e76dd29b59f2c0c191edfae89
Rafael Gomez <rgomez2014@gmail.com>
2023-09-25 21:50:44 -0500

    added .js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homepage');
+const homeRoutes = require('./homepage.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
