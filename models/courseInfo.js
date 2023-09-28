6a7af3eb8bbe99afc944b5eeb67d99477b3eb247
Rafael Gomez <rgomez2014@gmail.com>
2023-09-28 09:53:19 -0500

    fixed heroku connection

--- models/courseInfo.js
+++ models/courseInfo.js
@@ -12,7 +12,7 @@ CourseInfo.init(
             autoIncrement: true,
         },
         name: {
-            type: DataTypes.TEXT,
+            type: DataTypes.STRING,
             allowNull: false,
             unique:true,
         },
