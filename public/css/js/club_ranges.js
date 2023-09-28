53d98c62bd532fed5a8d7adaee1e8451fb207d4b
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-09-28 13:04:24 -0500

    updated CSS and js
    added input validation for user entry. Also updated the css to make page easier to read

--- public/css/js/club_ranges.js
+++ public/css/js/club_ranges.js
@@ -7,12 +7,20 @@ function updateClubInfo(e) {
     let txtBx = parentLI.children('#club-range');
     let rangeDisplay = parentLI.children('#current-range');
     let clubRange = txtBx.val();
+    let clubRangeIntChecker = parseInt(txtBx.val());
 
-    rangeDisplay.text(`${clubRange} yrds`);
-    txtBx.val('');
+    //input validation
+    if (txtBx.val() === "" || isNaN(clubRangeIntChecker) || clubRangeIntChecker < 0)
+    {
+        clubRange = 0;
+        rangeDisplay.text(`Must be a postive number!`);
+        txtBx.val('');
+    }
+    else{
+        rangeDisplay.text(`${clubRange} yrds`);
+        txtBx.val('');
+    }
     
-    console.log(rangeDisplay.text());
-    console.log(clubRange);
 }
 
 
