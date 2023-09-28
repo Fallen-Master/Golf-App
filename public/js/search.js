97553c7bb0230874155e83ebf57659291003b789
Olivia Schif <131719296+livschif@users.noreply.github.com>
2023-09-28 17:07:31 -0500

    start round functionality and handlebars file

--- public/js/search.js
+++ public/js/search.js
@@ -1,38 +1,71 @@
-const searchBar = document.getElementById("searchBar");
-const results = document.getElementById('results');
+const options = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': 'a41af16ff5msh7a6c6e9df83c957p184bbajsncd5a40f520fb',
+		'X-RapidAPI-Host': 'golf-course-api.p.rapidapi.com'
+	}
+};
 
-function displayCourseResults(){ //functions to display recipes to HTML
+function getAPI() {
+    var searchBar = $("#search-bar")
+    var query = searchBar.val();
+    console.log(query);
+    var requestUrl = 'https://golf-course-api.p.rapidapi.com/search?name=' + query;
+    fetch(requestUrl, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            
+            localStorage.setItem('courseResult', "");
+            localStorage.setItem('courseResult', JSON.stringify(data));
+        });
 
-   var courseData = JSON.parse(localStorage.getItem('courseResult')); 
-
-   var data;
-   if (courseData && courseData.length > 0){data=courseData}
-   
-    var resultsContainer = $('#displayResult');
-    resultsContainer.empty();
-    if (data && data.length > 0) { 
-      // function to dynamically add the container and the styling to the HTML
-      data.forEach(function(course) {
-        if (course.name){var titleName = `<h1 class="title has-text-weight-bold card-title">${course.name}
-        </h1>`} else { var titleName = 
-          `<h1 class="title has-text-weight-bold card-title">${course.title} 
-          </h1>`}
-          //displays recipe instructions and ingredients for both food and drinks pages
-        var displayCourseHTML = `
-        <div class="card recipe-card mt-5">
-          <div class="card-content"> `+ titleName +`
-            <p class="subtitle card-website">${course.website}</p> 
-            <p class="card-address">${course.address}</p>
-            <p class="card-address">${course.contact}</p>
-            <p class="card-address">${course.hours}</p>
-          </div>
-          <footer class="card-footer">
-            <a class="card-footer-item has-text-black startBTN">Start Round</a>
-          </footer>
-        `;
-        resultsContainer.append(displayCourseHTML);
-      });
-    } else {
-        resultsContainer.append('<p id="noCourses"></p>');
     }
-  }
+
+function searchBtn(e){
+    console.log("hello");
+    e.preventDefault();
+    getAPI();
+  displayCourseResults();  
+}
+
+
+function displayCourseResults(){
+    
+    var courseData = JSON.parse(localStorage.getItem('courseResult')); 
+    var data;
+    if (courseData && courseData.length > 0){data=courseData}
+    var resultsContainer = $('#displayResult');
+     resultsContainer.empty();
+     if (data && data.length > 0) { 
+         // function to dynamically add the container and the styling to the HTML
+         data.forEach(function(course) {
+             if (course.name){var titleName = `<h1 class="title has-text-weight-bold card-title">${course.name}
+             </h1>`} else { var titleName = 
+                `<h1 class="title has-text-weight-bold card-title">${course.title} 
+                </h1>`}
+                var displayCourseHTML = `
+                <div class="card recipe-card mt-5">
+                <div class="card-content"> `+ titleName +`
+                <p class="subtitle card-website">${course.website}</p> 
+                <p class="card-address">${course.address}</p>
+                <p class="card-address">${course.phone}</p>
+                <p class="card-address">${course.holes}</p>
+                </div>
+                <footer class="card-footer">
+                <div class="buttons is-centered">
+                      <button id="start-round">Start Round</button>
+                  </div>
+                </footer>
+                <script src="./public/js/startbtn.js" rel="javascript"></script>
+                `;
+                resultsContainer.append(displayCourseHTML);
+            });
+        } else {
+            resultsContainer.append('<p id="noCourses"></p>');
+        }
+    }
+    
+    $('#search-course').on('click',searchBtn)
