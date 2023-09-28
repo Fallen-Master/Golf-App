f247081fc6d9af932836bac74371b3d91186d31e
Rafael Gomez <rgomez2014@gmail.com>
2023-09-28 11:35:17 -0500

    remade homepage

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
-const { Club, User, Round } = require('../models');
+const { CourseInfo, User, Comment } = require('../models')
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   res.render('homepage');
 });
-router.get('/')
-// if user is logged in it takes them to the thier history
+router.post('/search', withAuth, async (req, res) => {
+  try {
+    const { searchQuery } = req.body;
+    
+    // Perform a search query based on user input
+    const courses = await Club.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${searchQuery}%`, // Use case-insensitive search
+        },
+      },
+    });
+
+    res.render('homepage', { courses, user: req.session.user });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get('/login', (req, res) => { 
   if (req.session.logged_in) {
     res.redirect('/hompage');
